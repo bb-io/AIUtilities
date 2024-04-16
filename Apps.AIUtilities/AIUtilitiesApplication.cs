5e2e084bfc378acbfdbfa83075cb156abe8a911c
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-16 18:51:08 +0300

    Categories (#11)
    * added categories
    
    * Update Apps.AIUtilities.csproj

--- Apps.AIUtilities/AIUtilitiesApplication.cs
+++ Apps.AIUtilities/AIUtilitiesApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.AIUtilities;
 
-public class AiUtilitiesApplication : IApplication
+public class AiUtilitiesApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.ArtificialIntelligence, ApplicationCategory.Utilities];
+        set { }
+    }
+    
     public string Name
     {
         get => "AI Utilities";
